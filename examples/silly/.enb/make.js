34cb04161b8713ac42b8408be4956420a0b98fbf
Evgeniy Baranov <master@kompolom.ru>
2015-09-23 17:25:36 +0300

    add sample spec congig for BH.js

--- examples/silly/.enb/make.js
+++ examples/silly/.enb/make.js
@@ -16,4 +16,29 @@ module.exports = function (config) {
             { path: 'blocks', check: true }
         ]
     });
+
+    var specsBH = config.module('enb-bem-specs').createConfigurator('specs-bh');
+
+    specsBH.configure({
+        langs: true,
+        destPath: 'set.specs',
+        levels: ['blocks'],
+        engine: {
+            name: 'bh',
+            tech: require('enb-bh/techs/bh-bundle'),
+            bemjsonTech: require('enb-bh/techs/bemjson-to-html'),
+            options: {
+                mimic: 'BEMHTML',
+                bhOptions: {
+                    jsAttrName: 'data-bem',
+                    jsAttrScheme: 'json'
+                }
+            },
+        },
+        sourceLevels: [
+            { path: '../libs/bem-core/common.blocks', check: false },
+            { path: '../libs/bem-pr/spec.blocks', check: false },
+            { path: 'blocks', check: true }
+        ]
+    });
 };
