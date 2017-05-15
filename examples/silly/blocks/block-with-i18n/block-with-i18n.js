52ccda2880b863943390d59af4474ff450ad2a1a
Aleksandr Fursenko <syngularity@ya.ru>
2017-05-15 14:33:09 +0300

    Fastering build by clearing some packages

--- examples/silly/blocks/block-with-i18n/block-with-i18n.js
+++
@@ -1,9 +0,0 @@
-modules.define('block-with-i18n', ['i18n'], function(provide, i18n) {
-
-    provide({
-        foo: function () {
-            return i18n('block-with-i18n','bar');
-        }
-    });
-
-});
--- examples/silly/blocks/block-with-i18n/block-with-i18n.spec.js
+++
@@ -1,9 +0,0 @@
-modules.define('spec', ['block-with-i18n'], function(provide, block) {
-
-    it('should be equal to one', function() {
-        block.foo().should.eql('block-with-i18n:bar');
-    });
-
-    provide();
-
-});
